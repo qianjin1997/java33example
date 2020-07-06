1cd13e98abe5bcc69c6f178eae0ec8cd2c5f78be
加油，前进 <@DESKTOP-PFT48TN>
2020-07-06 22:24:16 +0800

    第一次修改

--- example2/src/main/java/com/accp/controller/StudentController.java
+++ example2/src/main/java/com/accp/controller/StudentController.java
@@ -1,5 +1,15 @@
 package com.accp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.accp.service.StudentService;
+
+@RestController
+@RequestMapping("/student")
 public class StudentController {
 
+	@Autowired
+	private StudentService service;
 }
--- example2/src/main/java/com/accp/service/StudentService.java
+++ example2/src/main/java/com/accp/service/StudentService.java
@@ -1,5 +1,15 @@
 package com.accp.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.accp.dao.StudentMapper;
+
+@Service
+@Transactional
 public class StudentService {
 
+	@Autowired
+	private StudentMapper mapper;
 }
